5d51bdee4ee6b84a1270de6d13cf28b74893f9a5
Sven Schwarz <me@MacBook-Air-von-Sven.local>
2024-12-27 14:31:23 +0100

    deleted:    __init__.py 	deleted:    avreceiver.py 	deleted:    const.py 	deleted:    number.py 	deleted:    switch.py

--- custom_components/iotavx_avx1/__init__.py
+++
@@ -1,9 +0,0 @@
-from .avreceiver import AVReceiver
-
-DOMAIN = "iotavx_avx1"
-
-async def async_setup(hass, config):
-    """Set up the IOTAVX AVX1 integration."""
-    hass.data[DOMAIN] = {}
-    return True
-
--- custom_components/iotavx_avx1/const.py
+++
@@ -1,2 +0,0 @@
-DOMAIN = "iotavx_avx1"
-
